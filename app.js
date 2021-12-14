90f46b3651251606c81b54eb6f036b07dd265c46
kelvin <okorieebube1@gmail.com>
2021-12-14 13:52:12 +0100

    fix: minor fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 //EXPRESS CONFIGURE
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('./public/images'))
 
@@ -34,9 +34,9 @@ app.set('view engine', 'ejs');
 //SETTING UP SESSION
 app.use(session({
     secret: config.sessionSecrete,
-    saveUninitialized:true,
-    resave:true,
-})); 
+    saveUninitialized: true,
+    resave: true,
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -54,7 +54,7 @@ app.use('/to-switch/', adminRoutes);
 
 
 //SERVER CONFIGURATION
-const server = app.listen(config.PORT, ()=> {
+const server = app.listen(config.PORT, () => {
     console.log(`App server listening on Port: ${config.PORT}`)
 })
 
@@ -65,215 +65,215 @@ const connectedSockets = {};
 io.on('connection', (socket) => {
     // console.log(socket);
 
-    socket.on('join',(data) => {
+    socket.on('join', (data) => {
         socket.join(data.room);
 
-        mySqlQuery.query('SELECT * from conversations where ?', {unique_id: data.room}, function(err, rows)   {
-            if(err) throw err;
+        mySqlQuery.query('SELECT * from conversations where ?', { unique_id: data.room }, function (err, rows) {
+            if (err) throw err;
             //   console.log(rows);
-            if(rows.length !== 0){
+            if (rows.length !== 0) {
                 connectedSockets[data.room] = socket;
-                mySqlQuery.query('SELECT * from messages where ?', {conversation_id: data.room}, function(err, rows)  {
-                    if(err) throw err;
+                mySqlQuery.query('SELECT * from messages where ?', { conversation_id: data.room }, function (err, rows) {
+                    if (err) throw err;
                     //   console.log(rows);
                     rows.forEach(chat => {
                         // console.log(chat);
                         socket.emit('prev-chat-message', [chat]);
                     })
-                    
-                    
+
+
                 });
-            }else{
+            } else {
                 console.log('Server error!');
             }
-            
+
         });
         socket.on('chat-message', (msg) => {
             // console.log( msg ); 
             let message = {
-                conversation_id:msg.to.room,
-                message:msg.msg,
-                sender:msg.to.onlineUser,
-                reciever:msg.to.reciever,
-                time_created:lib.createDate(),
-                unique_id:'MSG'+lib.genRandomNo()+''+lib.genRandomNo(),
+                conversation_id: msg.to.room,
+                message: msg.msg,
+                sender: msg.to.onlineUser,
+                reciever: msg.to.reciever,
+                time_created: lib.createDate(),
+                unique_id: 'MSG' + lib.genRandomNo() + '' + lib.genRandomNo(),
             };
             mySqlQuery.query('INSERT INTO messages SET ?', message, (err, res) => {
-             if(err) throw err;
-            //  console.log(res);
-             if(res.affectedRows == 1){
-                socket.to(msg.to.room).emit('chat-message', message); 
+                if (err) throw err;
+                //  console.log(res);
+                if (res.affectedRows == 1) {
+                    socket.to(msg.to.room).emit('chat-message', message);
 
-            }else{
-                console.log('Database Error');
-            }
+                } else {
+                    console.log('Database Error');
+                }
 
-           }) 
+            })
         });
 
     })
     socket.on('notification', async (notificationId) => {
-            // console.log(notificationId);
-            let notifications = await lib.selectQuery('SELECT * from notifications where ?','with_params',{uniqueId: notificationId[0]});
-            let viewers = notifications[0].viewed_by;
-            let viewed_user_arr = viewers.split(',');
-            viewed_user_arr.push(notificationId[1]);
-            let joined_viewed_user_arr = viewed_user_arr.join(',');
-            mySqlQuery.query('UPDATE notifications set viewed_by = ?  WHERE uniqueId = ?', [joined_viewed_user_arr,notificationId[0]], (err, res) => {
-                if(err) throw err;
-                // console.log(rows);
-                socket.emit('notification-detail', notifications);
-                
-    
-            }) 
-        
+        // console.log(notificationId);
+        let notifications = await lib.selectQuery('SELECT * from notifications where ?', 'with_params', { uniqueId: notificationId[0] });
+        let viewers = notifications[0].viewed_by;
+        let viewed_user_arr = viewers.split(',');
+        viewed_user_arr.push(notificationId[1]);
+        let joined_viewed_user_arr = viewed_user_arr.join(',');
+        mySqlQuery.query('UPDATE notifications set viewed_by = ?  WHERE uniqueId = ?', [joined_viewed_user_arr, notificationId[0]], (err, res) => {
+            if (err) throw err;
+            // console.log(rows);
+            socket.emit('notification-detail', notifications);
+
+
+        })
+
     })
 
-    socket.on('get-sub-categories',async (cat_id)=> {
-        let all_sub_categories = await lib.selectQuery('SELECT * from sub_category where linked_to = ?','with_params',[cat_id]);
+    socket.on('get-sub-categories', async (cat_id) => {
+        let all_sub_categories = await lib.selectQuery('SELECT * from sub_category where linked_to = ?', 'with_params', [cat_id]);
         // console.log(all_sub_categories);
-        socket.emit('return-sub-categories',all_sub_categories);
+        socket.emit('return-sub-categories', all_sub_categories);
 
     })
-    
+
     socket.on('chat-message', (msg) => {
         // console.log( msg ); 
         let message = {
-            conversation_id:msg.to.room,
-            message:msg.msg,
-            sender:msg.to.onlineUser,
-            reciever:msg.to.reciever,
-            time_created:lib.createDate(),
-            unique_id:'MSG'+lib.genRandomNo()+''+lib.genRandomNo(),
+            conversation_id: msg.to.room,
+            message: msg.msg,
+            sender: msg.to.onlineUser,
+            reciever: msg.to.reciever,
+            time_created: lib.createDate(),
+            unique_id: 'MSG' + lib.genRandomNo() + '' + lib.genRandomNo(),
         };
         mySqlQuery.query('INSERT INTO messages SET ?', message, (err, res) => {
-         if(err) throw err;
-        //  console.log(res);
-         if(res.affectedRows == 1){
-            socket.to(msg.to.room).emit('chat-message', message); 
+            if (err) throw err;
+            //  console.log(res);
+            if (res.affectedRows == 1) {
+                socket.to(msg.to.room).emit('chat-message', message);
 
-        }else{
-            console.log('Database Error');
-        }
+            } else {
+                console.log('Database Error');
+            }
 
-       }) 
+        })
     });
 
-    
 
-    socket.on('join-support',(data) => {
+
+    socket.on('join-support', (data) => {
         socket.join(data.room);
 
-        mySqlQuery.query('SELECT * from admin_support where ?', {conversation_id: data.room}, function(err, rows)   {
-            if(err) throw err;
+        mySqlQuery.query('SELECT * from admin_support where ?', { conversation_id: data.room }, function (err, rows) {
+            if (err) throw err;
             //   console.log(rows);
-            if(rows.length !== 0){
+            if (rows.length !== 0) {
                 connectedSockets[data.room] = socket;
-                mySqlQuery.query('SELECT * from admin_support_messages where ?', {conversation_id: data.room}, function(err, rows)  {
-                    if(err) throw err;
+                mySqlQuery.query('SELECT * from admin_support_messages where ?', { conversation_id: data.room }, function (err, rows) {
+                    if (err) throw err;
                     //   console.log(rows);
                     rows.forEach(chat => {
                         // console.log(chat);
                         socket.emit('prev-admin-message', [chat]);
                     })
-                    
-                    
+
+
                 });
-            }else{
+            } else {
                 console.log('Server error!');
 
             }
-            
+
         });
         socket.on('support-message', (msg) => {
             // console.log( msg ); 
             let message = {
-                conversation_id:msg.to.room,
-                reciever:'admin',
-                sender:msg.to.onlineUser,
-                message:msg.msg,
-                read_status:'unread',
-                date_sent:lib.createDate(),
-                message_uniq_id:'ADMINSUPPORTMSG'+lib.genRandomNo()+''+lib.genRandomNo(),
+                conversation_id: msg.to.room,
+                reciever: 'admin',
+                sender: msg.to.onlineUser,
+                message: msg.msg,
+                read_status: 'unread',
+                date_sent: lib.createDate(),
+                message_uniq_id: 'ADMINSUPPORTMSG' + lib.genRandomNo() + '' + lib.genRandomNo(),
             };
             mySqlQuery.query('INSERT INTO admin_support_messages SET ?', message, (err, res) => {
-             if(err) throw err;
-            //  console.log(res);
-             if(res.affectedRows == 1){
-                socket.to(msg.to.room).emit('admin-support-message', message); 
-                socket.to('adminGeneralSocket').emit('new-message-notify', message.sender); 
+                if (err) throw err;
+                //  console.log(res);
+                if (res.affectedRows == 1) {
+                    socket.to(msg.to.room).emit('admin-support-message', message);
+                    socket.to('adminGeneralSocket').emit('new-message-notify', message.sender);
 
-            }else{
-                console.log('Database Error');
-            }
+                } else {
+                    console.log('Database Error');
+                }
 
-           }) 
+            })
         });
 
     })
 
-    socket.on('join-admin',(data) => {
+    socket.on('join-admin', (data) => {
         socket.join(data.room);
 
-        mySqlQuery.query('SELECT * from admin_support where ?', {conversation_id: data.room}, function(err, rows)   {
-            if(err) throw err;
+        mySqlQuery.query('SELECT * from admin_support where ?', { conversation_id: data.room }, function (err, rows) {
+            if (err) throw err;
             //   console.log(rows);
-            if(rows.length !== 0){
+            if (rows.length !== 0) {
                 connectedSockets[data.room] = socket;
-                mySqlQuery.query('SELECT * from admin_support_messages where ?', {conversation_id: data.room}, function(err, rows)  {
-                    if(err) throw err;
+                mySqlQuery.query('SELECT * from admin_support_messages where ?', { conversation_id: data.room }, function (err, rows) {
+                    if (err) throw err;
                     //   console.log(rows);
                     rows.forEach(chat => {
                         // console.log(chat);
                         socket.emit('prev-admin-support', [chat]);
                     })
-                    
-                    
+
+
                 });
-            }else{
+            } else {
                 console.log('Server error!');
 
             }
-            
+
         });
         socket.on('admin-support-message', (msg) => {
             // console.log( msg ); 
             // return;
             let message = {
-                conversation_id:msg.to.room,
-                sender:msg.to.onlineAdmin,
-                reciever:msg.to.reciever,
-                message:msg.msg,
-                read_status:'unread',
-                date_sent:lib.createDate(),
-                message_uniq_id:'ADMINSUPPORTMSG'+lib.genRandomNo()+''+lib.genRandomNo(),
+                conversation_id: msg.to.room,
+                sender: msg.to.onlineAdmin,
+                reciever: msg.to.reciever,
+                message: msg.msg,
+                read_status: 'unread',
+                date_sent: lib.createDate(),
+                message_uniq_id: 'ADMINSUPPORTMSG' + lib.genRandomNo() + '' + lib.genRandomNo(),
             };
             mySqlQuery.query('INSERT INTO admin_support_messages SET ?', message, (err, res) => {
-             if(err) throw err;
-            //  console.log(res);
-             if(res.affectedRows == 1){
-                mySqlQuery.query('UPDATE admin_support_messages set read_status = ?  WHERE conversation_id = ?', ['read',msg.to.room], (err, res) => {
-                    if(err) throw err;
-                    socket.to(msg.to.room).emit('support-message', message); 
-                }) 
-                
+                if (err) throw err;
+                //  console.log(res);
+                if (res.affectedRows == 1) {
+                    mySqlQuery.query('UPDATE admin_support_messages set read_status = ?  WHERE conversation_id = ?', ['read', msg.to.room], (err, res) => {
+                        if (err) throw err;
+                        socket.to(msg.to.room).emit('support-message', message);
+                    })
 
-            }else{
-                console.log('Database Error');
-            }
 
-           }) 
+                } else {
+                    console.log('Database Error');
+                }
+
+            })
         });
 
     })
 
-    socket.on('other-admin-view', ()=>{
+    socket.on('other-admin-view', () => {
         socket.join('adminGeneralSocket');
         connectedSockets['adminGeneralSocket'] = socket;
         // console.log(connectedSockets)
     })
 
-    
+
 
 
 
--- config/lib.js
+++ config/lib.js
@@ -1,106 +1,106 @@
-const  express = require('express');
+const express = require('express');
 const router = express.Router();
 const mySqlQuery = require('../mysql');
 const path = require('path');
 const passport = require('passport');
 
 const lib = {
-    selectOptions : function(status, options) {
+    selectOptions: function (status, options) {
         return options.fn(this).replace(
-            new RegExp('value=\"'+status+'\"'),
-             '$&selected="selected"');
+            new RegExp('value=\"' + status + '\"'),
+            '$&selected="selected"');
     },
 
-    isEmpty: function(obj) {
-        for(let key in obj){
-            if(obj.hasOwnProperty(key)){
+    isEmpty: function (obj) {
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
                 return false;
             }
         }
         return true;
-    },    
+    },
 
-    isUserAuthenticated: (req,res,next)=> {
-        if(req.isAuthenticated()){
+    isUserAuthenticated: (req, res, next) => {
+        if (req.isAuthenticated()) {
             next();
-        }else{
+        } else {
             res.redirect('/');
         }
     },
-    genRandomNo:()=> {
-        let ini = ['RPC34','RPD32','RPD56','RPE89','RPF87','RPG76','RPH23','RPI78','RPJ54','RPK45','RPL90','RPM43','RPN43','RPO56','RPP67','RPQ78','RPR43','RPS76','RPT34','RPU45','RPV67','RPW78','RPX56','RPY67','RPZ34','RRR09','REA90','REB56','REC54','RED67','REE78','REF54','REG','REH56','REI56','REJ34','REK87','REL56','REM54','REN45','REO43','REP78','REQ67','RER43','RES45','RET34','REU34','REV65','REW56','REX56','REY78','REZ43','RDA65','RDB67','RDC34','RDD23','RDE87',"RAA87","RBH54","RHJ65","RKK45","RWH43","RBB45","RFC67","RGC54","RHC90","RJC43","RKC67","TLC34","TZC54","TXC34","TCC34","TVC67","TBC54","TNC54","TDO56","TDT67","TTT45","TAG54","TAH34","TAS54","TAR45","TAC78","TAT67","TAZ34","TSY54","TSB54","TZX78","TQO65",];
+    genRandomNo: () => {
+        let ini = ['RPC34', 'RPD32', 'RPD56', 'RPE89', 'RPF87', 'RPG76', 'RPH23', 'RPI78', 'RPJ54', 'RPK45', 'RPL90', 'RPM43', 'RPN43', 'RPO56', 'RPP67', 'RPQ78', 'RPR43', 'RPS76', 'RPT34', 'RPU45', 'RPV67', 'RPW78', 'RPX56', 'RPY67', 'RPZ34', 'RRR09', 'REA90', 'REB56', 'REC54', 'RED67', 'REE78', 'REF54', 'REG', 'REH56', 'REI56', 'REJ34', 'REK87', 'REL56', 'REM54', 'REN45', 'REO43', 'REP78', 'REQ67', 'RER43', 'RES45', 'RET34', 'REU34', 'REV65', 'REW56', 'REX56', 'REY78', 'REZ43', 'RDA65', 'RDB67', 'RDC34', 'RDD23', 'RDE87', "RAA87", "RBH54", "RHJ65", "RKK45", "RWH43", "RBB45", "RFC67", "RGC54", "RHC90", "RJC43", "RKC67", "TLC34", "TZC54", "TXC34", "TCC34", "TVC67", "TBC54", "TNC54", "TDO56", "TDT67", "TTT45", "TAG54", "TAH34", "TAS54", "TAR45", "TAC78", "TAT67", "TAZ34", "TSY54", "TSB54", "TZX78", "TQO65",];
 
         let rand = ini[Math.floor(Math.random() * ini.length)];
 
         rand += Math.floor(Math.random() * (10000000 - 1000000)) + 1000000;
         return rand;
     },
-    
-    checkFile: (req,file, cb) => {
-            // console.log(file)
+
+    checkFile: (req, file, cb) => {
+        // console.log(file)
         let fileTypes = /jpeg|jpg|png|gif/;
 
         let extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
 
         let mimeType = fileTypes.test(file.mimetype);
 
-        if(mimeType && extName ) {
-            return cb(null,true);
-        } else{
-            
-            cb({message:'Please select a valid file type!'});
+        if (mimeType && extName) {
+            return cb(null, true);
+        } else {
+
+            cb({ message: 'Please select a valid file type!' });
         }
-        
-        
-        
-      
-      },
-      createDate:() => {
+
+
+
+
+    },
+    createDate: () => {
         let date_obj = new Date();
         let current_date = date_obj.toUTCString();
         let date_arr = current_date.split(" ");
         let timearr = date_arr[4].split(":");
-        if(Number(timearr[0]) > 12){
+        if (Number(timearr[0]) > 12) {
             let meridian = 'pm';
-            let the_date = date_arr[0]+' '+date_arr[1]+' '+date_arr[2]+' '+date_arr[3]+' '+date_arr[4]+' '+meridian;
+            let the_date = date_arr[0] + ' ' + date_arr[1] + ' ' + date_arr[2] + ' ' + date_arr[3] + ' ' + date_arr[4] + ' ' + meridian;
             return the_date;
-        }else{
+        } else {
             let meridian = 'am';
-            let the_date = date_arr[0]+' '+date_arr[1]+' '+date_arr[2]+' '+date_arr[3]+' '+date_arr[4]+' '+meridian;
+            let the_date = date_arr[0] + ' ' + date_arr[1] + ' ' + date_arr[2] + ' ' + date_arr[3] + ' ' + date_arr[4] + ' ' + meridian;
             return the_date;
         }
 
-      },
-      selectQuery:(query,option,values)=>{
-          if(option == 'with_params'){
-            return new Promise((resolve,reject)=>{
+    },
+    selectQuery: (query, option, values) => {
+        if (option == 'with_params') {
+            return new Promise((resolve, reject) => {
 
-                mySqlQuery.query(query, values,(err,result, rows) => {
-                            if (err) {
-                                reject(err);    
-                            } else {
-                                resolve(result);
-                            }
+                mySqlQuery.query(query, values, (err, result, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
                 })
 
             })
-        }else if(option == 'all_rows'){
-            return new Promise((resolve,reject)=>{
-                mySqlQuery.query(query, (err,result, rows) => {
-                            if (err) {
-                                reject(err);    
-                            } else {
-                                resolve(result);
-                            }
+        } else if (option == 'all_rows') {
+            return new Promise((resolve, reject) => {
+                mySqlQuery.query(query, (err, result, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
                 })
 
             })
 
         }
 
-      },
+    },
 
-    
+
 
 
 };
